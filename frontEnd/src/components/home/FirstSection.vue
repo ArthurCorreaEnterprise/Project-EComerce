a126aaec21dc0ea8978cd245d80c881dea9c28c1
ArthurCorreaEnterprise <arthur.correa@DSS.LOCAL>
2024-10-26 00:08:04 -0400

    first day

--- frontEnd/src/components/home/FirstSection.vue
+++ frontEnd/src/components/home/FirstSection.vue
@@ -1,17 +1,16 @@
 <template>
-    <div class="style-00">
-        <div class="style-01">
+    <div class="section-container">
+        <div class="text-content">
             <h1>Modern Minimalist Furniture</h1>
             <p>Discover a curated collection of handcrafted pieces designed to transform your living spaces into
                 expressions of your unique taste and lifestyle.</p>
             <button type="button">Explore More</button>
         </div>
-        <div class="style-02">
-            <div class="style-03">
-                <SwiperPagination class="style-05" :images="images" />
+        <div class="image-content">
+            <div class="swiper-container">
+                <SwiperPagination class="pagination" :images="images" />
             </div>
-            <div class="style-04">
-            </div>
+            <div class="highlighted-image"></div>
         </div>
     </div>
 </template>
@@ -24,44 +23,40 @@ const images = [
     'images/image-slide-1.jpg',
     'images/image-slide-1.jpg',
 ];
-
 </script>
 
 <style scoped>
-.style-00 {
-    @apply grid grid-rows-2 p-4 gap-4
+.section-container {
+    @apply grid grid-rows-2 p-4 gap-4;
 }
 
-.style-01 {
-    @apply row-span-1 px-60 flex flex-col justify-center items-end rounded-lg gap-2 max-h-96;
-    @apply bg-cyan-600;
+.text-content {
+    @apply row-span-1 px-60 flex flex-col justify-center items-end rounded-lg gap-2 max-h-96 bg-cyan-600;
 }
 
-.style-02 {
-    @apply grid grid-cols-3 row-span-1 gap-4 
+.image-content {
+    @apply grid grid-cols-3 row-span-1 gap-4;
 }
 
-.style-03 {
-    @apply col-span-2 max-h-96 rounded-lg
+.swiper-container {
+    @apply col-span-2 max-h-96 rounded-lg;
 }
 
-.style-04 {
+.highlighted-image {
     @apply col-span-1 bg-cover bg-center rounded-lg;
-    background-image: url(/images/image-01.jpg)
+    background-image: url(/images/image-01.jpg);
 }
 
-.style-05 {
-    @apply rounded-lg
+.pagination {
+    @apply rounded-lg;
 }
 
 h1 {
-    @apply text-6xl font-bold w-full text-center;
-    @apply text-white;
+    @apply text-6xl font-bold w-full text-center text-white;
 }
 
 p {
-    @apply text-sm font-light w-1/2;
-    @apply text-white;
+    @apply text-sm font-light w-1/2 text-white;
 }
 
 button {
--- frontEnd/src/views/HomeView.vue
+++ frontEnd/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
         <FirstSection />
+        <SecondSection />
     </div>
 </template>
 
 <script setup>
 import FirstSection from '../components/home/FirstSection.vue'
+import SecondSection from '../components/home/SecondSection.vue'
 </script>
 
 <style scoped>
